bd89fecb273a803285ac837dd41b1a2e7aa86905
henrifloris99 <henri.floris@gmail.com>
2026-07-08 17:37:31 -0700

    Update excel.js

--- excel.js
+++ excel.js
@@ -1,1 +1,42 @@
+function exportExcel(events) {
 
+    const worksheetData = [
+        [
+            "#",
+            "Record In",
+            "Record Out",
+            "Duration",
+            "Slug"
+        ]
+    ];
+
+    events.forEach((event, index) => {
+
+        worksheetData.push([
+            index + 1,
+            event.recordIn,
+            event.recordOut,
+            event.duration,
+            event.slug
+        ]);
+
+    });
+
+
+    const worksheet = XLSX.utils.aoa_to_sheet(worksheetData);
+
+    const workbook = XLSX.utils.book_new();
+
+    XLSX.utils.book_append_sheet(
+        workbook,
+        worksheet,
+        "EDL Slugs"
+    );
+
+
+    XLSX.writeFile(
+        workbook,
+        "EDL_Slug_Report.xlsx"
+    );
+
+}
